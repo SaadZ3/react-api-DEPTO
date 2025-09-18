455162c6c9d57839940cc8e3b6590ebd09ed3f0f
SaadZ3 <saadmu139@gmail.com>
2025-09-18 00:18:37 -0300

    criando component consultar

--- src/pages/consultar/index.js
+++ src/pages/consultar/index.js
@@ -1,8 +1,15 @@
 // import { Link } from 'react-router-dom'; // isso é necessário para usar o Link do react-router-dom
+import { useState } from 'react';
 import { Container } from '../../styles/globalStyles';
 import { ConsultarContainer } from './styled';
 
 export default function Home() {
+   const [valorInput, setValorInput] = useState('');
+
+   const handleInputChange = (event) => {
+      setValorInput(event.target.value);
+   };
+
    return (
       <Container>
          <ConsultarContainer>
@@ -13,6 +20,8 @@ export default function Home() {
                // eslint-disable-next-line jsx-a11y/no-autofocus
                autoFocus="true"
                placeholder="Digite o nome do departamento"
+               value={valorInput}
+               onChange={handleInputChange}
             />
             <button type="button">Consultar</button>
          </ConsultarContainer>
